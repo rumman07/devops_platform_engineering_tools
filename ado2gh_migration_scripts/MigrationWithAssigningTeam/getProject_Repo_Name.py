0fab047ef882793a0851ca7ecc4a1f0d447e94fb
rumman07 <rummanahmed.ra@gmail.com>
2024-01-16 08:19:15 -0800

    added ado2gh migration scripts

---
+++ ado2gh_migration_scripts/MigrationWithAssigningTeam/getProject_Repo_Name.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+df = pd.read_csv('latestCommitInfo.csv')
+data = {
+    'project_name' : df['project_name'].to_numpy(),
+    "repo_name" : df['repository_name'].to_numpy(),
+ }
+print("Getting Data From Latest Commit Info CSV")
+n_df = pd.DataFrame(data)
+
+n_df.to_csv('./latestCommitInfoProjectRepoNames.csv',index=False)
+print("Data Extracting Successfull....\n")
+print(n_df,end="\n\n\n\n")
