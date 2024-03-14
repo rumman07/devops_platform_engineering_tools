74cf93fbc61f6f501db6a973601dc8e223cd82fc
REZ-OAN <ahmedabir.rez@gmail.com>
2024-03-14 12:21:07 +0600

    resolved dependencies on another csv file for extracting the repo names and remove the pictorial representation

--- listingAllLanguages/listingRepositoryLanguageOnGithub/listingRepositoryLanguageOnGithub.py
+++ listingAllLanguages/listingRepositoryLanguageOnGithub/listingRepositoryLanguageOnGithub.py
@@ -6,20 +6,50 @@ import matplotlib.pyplot as plt
 ## get the current directory
 c_dir = os.path.dirname(os.path.realpath(__file__))
 
-## getting the organization Repositories which have migrated successfully to github
-path =f"{'/'.join(os.path.dirname(__file__).split('/')[0:-2])}"+'/ado2gh_migration_scripts/MigrationWithoutAssigningTeam/checkAfterMigration/orgRepositories.csv'
-
-repo_under_organization = pd.read_csv(path)
-repo_names = repo_under_organization['full_name'].to_numpy()
 ## ensuring if the error.log file exists or not
 if not os.path.exists(f'{c_dir}/error.log'):
     with open(f'{c_dir}/error.log', 'w'):
         pass
 
-
+def get_user_repositories(gh_pat,org_name):
+    try :
+        ## page no
+        i = int(1)
+        repositories_info = []
+        while True :
+            # request header
+            headers = {
+                "Accept": "application/vnd.github+json",
+                "Authorization": f"Bearer {gh_pat}",
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+            ## parameters to pass to the rest api
+            params={
+                "per_page":100,
+                "page":i,
+            }
+            url = f"https://api.github.com/orgs/{org_name}/repos"
+            response = requests.get(url, headers=headers,params=params)
+            
+            # getting the response into json format
+            results = response.json()
+            # if there is no repos in that page then break
+            if not results :
+                break
+            for repository in results :
+                repositories_info.append(
+                    
+                    repository['full_name']
+                );
+            i = i + 1
+        return repositories_info
+    except requests.exceptions.RequestException as e :
+        error_msg = f'Error occurred while fetching data : {e}'
+        print(error_msg)
+        logging.error(error_msg)
 ## configure logging file
 logging.basicConfig(filename=f'{c_dir}/error.log',level=logging.ERROR,format='%(asctime)s - %(levelname)s - %(message)s')
-def get_lang_in_repositories(gh_pat):
+def get_lang_in_repositories(gh_pat,repo_names):
     try :
         # request header
         columns = ['repo_name','languages_used']
@@ -46,20 +76,12 @@ def get_lang_in_repositories(gh_pat):
         logging.error(error_msg)
 
 
-def plot_tabular_data(df):
-    plt.figure(figsize=(12, 8))
-    plt.axis('off')
-    plt.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
-    plt.savefig(f'{c_dir}/tabular_data.pdf', format='pdf')  # Save as PDF
-    plt.savefig(f'{c_dir}/tabular_data.png', format='png')  # Save as PNG
-    plt.show()
-
 if __name__ == "__main__" :
 
     # GitHub Personal Access Token
     gh_pat = os.environ.get("GH_PAT")
     # Github User Name
-    org_name = "utesta"
-    df = get_lang_in_repositories(gh_pat)
+    org_name = "freeCodeCamp"
+    repo_names = get_user_repositories(gh_pat,org_name)
+    df = get_lang_in_repositories(gh_pat,repo_names)
     df.to_csv(f'{c_dir}/laguageStatusGitHub.csv',index=False)
-    plot_tabular_data(df)
